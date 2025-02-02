fd86980950d831c7f3a682c02a9491c016263211
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-02-02 12:53:35 +0100

    big bug : protect ft_strlen from NULL address

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 13:17:08 by mmoulati          #+#    #+#             */
-/*   Updated: 2024/11/10 13:18:19 by mmoulati         ###   ########.fr       */
+/*   Updated: 2025/02/02 12:52:24 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *str)
 {
 	size_t	i;
 
+	if (str == NULL)
+		return (0);
 	i = 0;
 	while (str[i])
 		i++;
