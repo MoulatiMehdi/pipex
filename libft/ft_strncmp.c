125d3c687b926af99354a3d365a7276463bb0373
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-02-02 13:28:19 +0100

    fix bug : protect ft_strncmp from NULL address

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 13:17:09 by mmoulati          #+#    #+#             */
-/*   Updated: 2024/11/10 13:18:20 by mmoulati         ###   ########.fr       */
+/*   Updated: 2025/02/02 13:27:46 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (s1 == s2)
+		return (0);
+	if (s1 == NULL || s2 == NULL)
+		return (1);
 	i = 0;
 	while (i < n && s1[i] && s2[i])
 	{
