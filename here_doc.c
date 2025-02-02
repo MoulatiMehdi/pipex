08bb874393c06849dc872f28796c98fe8f3f3442
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-02-02 18:59:32 +0100

    add files : heredocs

---
+++ here_doc.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   here_doc.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/02 11:51:30 by mmoulati          #+#    #+#             */
+/*   Updated: 2025/02/02 18:52:33 by mmoulati         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "config.h"
+#include "libft/libft.h"
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+static int	ft_heredoc_warn(char *delimiter)
+{
+	if (delimiter == NULL)
+		return (0);
+	ft_putstr_fd("\n" SHELL_NAME ": ", 2);
+	ft_putstr_fd("warning: here-document delimited by end-of-file ", 2);
+	ft_putstr_fd("(wanted `", 2);
+	ft_putstr_fd(delimiter, 2);
+	ft_putendl_fd("')", 2);
+	return (1);
+}
+
+static int	ft_heredoc_iseof(char *eof, char *p, int n)
+{
+	if (eof == p)
+		return (1);
+	return (ft_strncmp(p, eof, n) == 0 && p[n] == '\n' && p[n + 1] == '\0');
+}
+
+char	*ft_heredoc_getline(char *delimiter, int fd)
+{
+	char	*line;
+	char	*tmp;
+	char	*p;
+
+	line = NULL;
+	ft_putstr_fd(PROMPT_HERE_DOC, 1);
+	while (1)
+	{
+		errno = 0;
+		p = get_next_line(fd);
+		if (errno == ENOMEM)
+			break ;
+		if (line == NULL && p == NULL)
+			break ;
+		if (p != NULL)
+			ft_strconcat(&line, p);
+		if (p != NULL && ft_strchr(p, '\n'))
+			break ;
+		free(p);
+	}
+	free(p);
+	return (line);
+}
+
+int	ft_heredoc(char *delimiter, int fd_in, int fd_out)
+{
+	char	*line;
+	char	*text;
+	int		size;
+
+	if (delimiter == NULL)
+		return (1);
+	text = NULL;
+	size = ft_strlen(delimiter);
+	while (1)
+	{
+		line = ft_heredoc_getline(delimiter, fd_in);
+		if (delimiter && !line)
+		{
+			ft_heredoc_warn(delimiter);
+			break ;
+		}
+		if (ft_heredoc_iseof(delimiter, line, size))
+			break ;
+		ft_strconcat(&text, line);
+		free(line);
+	}
+	ft_putstr_fd(text, fd_out);
+	free(line);
+	free(text);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		return (0);
+	ft_heredoc(argv[1], 0, 1);
+}
