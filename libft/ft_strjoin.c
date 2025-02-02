ce8aa3e32b072fa87255ec8b94b6ebcf52267e24
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-02-02 12:25:44 +0100

    fix bug : ft_strjoin free a string that doesnt own

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 13:17:08 by mmoulati          #+#    #+#             */
-/*   Updated: 2024/11/13 21:08:37 by mmoulati         ###   ########.fr       */
+/*   Updated: 2025/02/02 12:13:13 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s1_len;
 	size_t	s2_len;
 
-	if (!s1 || !s2)
-		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
-	p = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	p = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!p)
 		return (NULL);
-	ft_memcpy(p, s1, s1_len);
-	ft_memcpy(p + s1_len, s2, s2_len);
 	p[s1_len + s2_len] = 0;
+	while (s2_len--)
+		p[s1_len + s2_len] = s2[s2_len];
+	while (s1_len--)
+		p[s1_len] = s1[s1_len];
 	return (p);
 }
