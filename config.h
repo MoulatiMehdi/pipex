e00b8475439d7849c6b8954092e6df84f56287ba
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-02-02 15:04:46 +0100

    add prompt_here_doc macro

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 14:58:36 by mmoulati          #+#    #+#             */
-/*   Updated: 2025/02/02 14:59:43 by mmoulati         ###   ########.fr       */
+/*   Updated: 2025/02/02 15:04:19 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 # define CONFIG_H
 
 # define SHELL_NAME "pipex"
+# define PROMPT_HERE_DOC ">"
 
 #endif
