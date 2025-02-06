9e36acfd456c28a54e6aa773d8221868ae11be8e
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-02-06 19:36:28 +0100

    add shell_error prototype functions

--- error.h
+++ error.h
@@ -6,7 +6,7 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 18:51:01 by mmoulati          #+#    #+#             */
-/*   Updated: 2025/02/06 18:52:44 by mmoulati         ###   ########.fr       */
+/*   Updated: 2025/02/06 19:35:53 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,8 @@
 # include <errno.h>
 # include <string.h>
 # include <unistd.h>
+
+void	ft_shell_error(char *str1, char *str2);
+int		ft_shell_perror(char *str);
+
 #endif
