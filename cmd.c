500b13645ea9fb90cf1026ddac2815360cab5c2f
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-02-07 17:19:48 +0100

    fix command exit code

--- cmd.c
+++ cmd.c
@@ -6,12 +6,13 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 20:08:28 by mmoulati          #+#    #+#             */
-/*   Updated: 2025/02/06 22:05:57 by mmoulati         ###   ########.fr       */
+/*   Updated: 2025/02/07 16:34:19 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cmd.h"
 #include "libft/libft.h"
+#include <unistd.h>
 
 int	ft_path_exec(char **strs)
 {
@@ -21,7 +22,7 @@ int	ft_path_exec(char **strs)
 	fullpath = ft_path_cmd(strs[0]);
 	if (fullpath == NULL)
 	{
-		ft_shell_error("command not found", strs[0]);
+		ft_shell_error(strs[0], "command not found");
 		return (errno = 127);
 	}
 	execve(fullpath, strs, NULL);
@@ -40,18 +41,17 @@ int	ft_cmd_exec(char *const cmd)
 	if (strs == NULL)
 		return (-1);
 	if (ft_strchr(strs[0], '/') == NULL)
-	{
 		ft_path_exec(strs);
-	}
 	else if (ft_path_isdir(strs[0]))
 	{
 		ft_shell_error(strs[0], strerror(21));
-		errno = 21;
+		errno = 126;
 	}
 	else
 	{
 		execve(strs[0], strs, NULL);
-		ft_shell_error(strerror(errno), strs[0]);
+		ft_shell_error(strs[0], strerror(errno));
+		errno = 126;
 	}
 	ft_split_free(&strs);
 	return (errno);
