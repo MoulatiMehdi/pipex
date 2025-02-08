bfb03eba69cd3a8f4dc499c556588e78e981e301
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-02-08 20:48:53 +0100

    remove unecessary header

--- error.h
+++ error.h
@@ -6,17 +6,18 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 18:51:01 by mmoulati          #+#    #+#             */
-/*   Updated: 2025/02/08 20:47:43 by mmoulati         ###   ########.fr       */
+/*   Updated: 2025/02/08 20:48:41 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_H
+
 # define ERROR_H
+
 # define SHELL_NAME "pipex"
 # define PROMPT_HERE_DOC "> "
 # define ARGS_SEPERATOR " \t"
 
-# include "config.h"
 # include "libft/libft.h"
 # include <errno.h>
 # include <string.h>
